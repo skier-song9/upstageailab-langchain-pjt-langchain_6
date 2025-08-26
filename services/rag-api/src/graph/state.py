e06d7209b88f0e03ea74fe5a892bc9049950d465
skier-song9 <0603paul@gmail.com>
2025-08-26 10:21:07 +0900

    feat: update rag codes

--- services/rag-api/src/graph/state.py
+++ services/rag-api/src/graph/state.py
@@ -3,7 +3,20 @@ from typing import TypedDict, List
 class GraphState(TypedDict):
     """Represents the state of our graph."""
     initial_query: str
-    sbp_found: bool
-    sbp_title: str
+    base_paper: str # 사용자가 읽은 논문
+    sbp_found: bool # sbp (selected base paper)
+    sbp_title: str # full title of sbp
     retrieved_docs: List[str]
     answer: str
+    ### 김정빈 ###
+
+
+    ### 송규헌 ###
+
+
+    ### 이나경 ###
+
+
+    ### 조선미 ###
+
+    ### 편아현 ###
